c3452787a2a4cf4a02ab95dc904c77124dedb5c5
Mostafa Didar <mostafadidar10@gmail.com>
2020-03-22 02:09:19 +0600

    I added randomint function

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,11 @@
+import random
+
 def main():
   #print('You rolled a die')
-  roll = 5
+  #roll = 5
+  #print(f'You rolled a {roll}')
+  roll = random.randint(1, 6)
   print(f'You rolled a {roll}')
-
+  #4
 if __name__== "__main__":
     main()
